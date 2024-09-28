e38897cc0d13c54e360e0ab0d6c7768c84b4f405
0xb-s <145866191+0xb-s@users.noreply.github.com>
2024-09-28 15:57:31 -0700

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event as CEvent},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{
+    error::Error,
+    io,
+    time::{Duration, Instant},
+};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    Terminal,
+};
+
+mod app;
+
+mod git;
+mod git_utils;
+mod tui_module;
+mod utils;
+
+use app::App;
+
+enum Event<I> {
+    Input(I),
+    Tick,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+  
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+ 
+    let mut app = App::new();
+
+
+    let tick_rate = Duration::from_millis(250);
+    let mut last_tick = Instant::now();
+
+    loop {
+        terminal.draw(|f| {
+            app.render(f);
+        })?;
+
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+
+        if event::poll(timeout)? {
+            match event::read()? {
+                CEvent::Key(key) => {
+                    if app.handle_input(key) {
+                        break;
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        if last_tick.elapsed() >= tick_rate {
+            last_tick = Instant::now();
+            app.on_tick();
+        }
+    }
+
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
